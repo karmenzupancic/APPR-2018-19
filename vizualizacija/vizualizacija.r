597e08d6a772b56ad52f96e0f572a11c2950d7c3
Karmen Zupančič <karmen.zupancic1@gmail.com>
2019-09-01 19:09:39 +0200

    nov graf

--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -1,8 +1,9 @@
 # 3. faza: Vizualizacija podatkov
 
-
+#graf svetovni napadi
 graf.svetovni.napadi <- ggplot(podatki_napadi %>% filter(drzava == "World")) + aes(x=leto, y=stevilo, group=drzava)+ geom_point() + geom_line()
 
+#graf procenti iraqa
 pomozna_tabela1 <- podatki_napadi %>% filter(drzava == "World")
 pomozna_tabela1$drzava <- NULL
 names(pomozna_tabela1)[names(pomozna_tabela1)=="stevilo"] <- "stevilo_svet"
@@ -16,5 +17,11 @@ names(pomozna_tabela1)[names(pomozna_tabela1)=="stevilo"] <- "stevilo_iraq"
 pomozna_tabela1 <- mutate(pomozna_tabela1, procent = stevilo_iraq / stevilo_svet)
 
 graf.iraq.procenti <- ggplot(pomozna_tabela1) + aes(x=leto, y=procent, group=TRUE) + geom_point() + geom_line()
+
+#graf žrtev proti poškodovanim
+
+tabela_zrtev_poskodovanih <-right_join(podatki_poskodbe, podatki_zrtve)
+
+graf_zrtev_poskodovanih <- ggplot(tabela_zrtev_poskodovanih %>% filter(drzava %in% c("Iraq", "Western Europe", "Eastern Europe"))) + aes(x=zrtve, y=poskodbe, color=drzava) + geom_point()
 
 
