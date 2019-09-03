b903c93b5f52ba79e0502e625a2014d278d63527
Karmen Zupančič <karmen.zupancic1@gmail.com>
2019-09-03 23:03:51 +0200

    novi grafi

--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -1,22 +1,46 @@
 # 3. faza: Vizualizacija podatkov
 
 #graf svetovni napadi
-graf.svetovni.napadi <- ggplot(podatki_napadi %>% filter(drzava == "World")) + aes(x=leto, y=stevilo, group=drzava)+ geom_point() + geom_line()
+graf.svetovni.napadi <- ggplot(podatki_napadi %>% filter(drzava == "World")) + aes(x=leto, y=stevilo, group=drzava)+ geom_point() + geom_line() + theme(panel.grid.major.y =element_line(color="red"))
 
-#graf procenti iraqa
+#graf evropski napadi
+graf.evropa.napadi <- ggplot(podatki_napadi %>% filter(drzava %in% c("Western Europe", "Eastern Europe"))) + aes(x=leto, y=stevilo, group=drzava) + geom_line(color="red")
+
+#graf napadov v iraqu
+graf.iraq.napadi <- ggplot(podatki_napadi %>% filter(drzava == "Iraq")) + aes(x=leto, y=stevilo, group=drzava) + geom_line() + geom_smooth()
+
+
+#graf procenti drzav Bliznjega vzhoda
 pomozna_tabela1 <- podatki_napadi %>% filter(drzava == "World")
 pomozna_tabela1$drzava <- NULL
 names(pomozna_tabela1)[names(pomozna_tabela1)=="stevilo"] <- "stevilo_svet"
 
-pomozna_tabela1 <- right_join(pomozna_tabela1, podatki_napadi %>% filter(drzava == "Iraq"))
+pomozna_tabela1 <- right_join(pomozna_tabela1, podatki_napadi %>% filter(drzava == "Middle East & North Africa"))
 
 
 pomozna_tabela1$drzava <- NULL
-names(pomozna_tabela1)[names(pomozna_tabela1)=="stevilo"] <- "stevilo_iraq"
+names(pomozna_tabela1)[names(pomozna_tabela1)=="stevilo"] <- "stevilo_vzhod"
 
-pomozna_tabela1 <- mutate(pomozna_tabela1, procent = stevilo_iraq / stevilo_svet)
+pomozna_tabela1 <- mutate(pomozna_tabela1, procent = stevilo_vzhod / stevilo_svet)
 
-graf.iraq.procenti <- ggplot(pomozna_tabela1) + aes(x=leto, y=procent, group=TRUE) + geom_point() + geom_line()
+graf.vzhod.procenti <- ggplot(pomozna_tabela1) + aes(x=leto, y=procent, group=TRUE) + geom_line() + geom_area()
+
+
+#graf procenti zahodno Evropskih drzav
+pomozna_tabela2 <- podatki_napadi %>% filter(drzava == "World")
+pomozna_tabela2$drzava <- NULL
+names(pomozna_tabela2)[names(pomozna_tabela2)=="stevilo"] <- "stevilo_svet"
+
+pomozna_tabela2 <- right_join(pomozna_tabela2, podatki_napadi %>% filter(drzava == "Western Europe"))
+
+
+pomozna_tabela2$drzava <- NULL
+names(pomozna_tabela2)[names(pomozna_tabela2)=="stevilo"] <- "stevilo_evropa"
+
+pomozna_tabela2 <- mutate(pomozna_tabela2, procent = stevilo_evropa / stevilo_svet)
+
+graf.evropa.procenti <- ggplot(pomozna_tabela2) + aes(x=leto, y=procent, group=TRUE) + geom_line() + geom_area()
+
 
 #graf žrtev proti poškodovanim
 
