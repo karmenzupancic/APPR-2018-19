c9eb8cfc7737e2bb10acc37344147c9e10110198
Karmen Zupančič <karmen.zupancic1@gmail.com>
2019-09-04 16:02:58 +0200

    zemljevid, dodana analiza

--- analiza/analiza.r
+++ analiza/analiza.r
@@ -1,11 +1,30 @@
 # 4. faza: Analiza podatkov
 
-podatki <- obcine %>% transmute(obcina, povrsina, gostota,
-                                gostota.naselij=naselja/povrsina) %>%
-  left_join(povprecja, by="obcina")
-row.names(podatki) <- podatki$obcina
-podatki$obcina <- NULL
+podatki <- right_join(podatki_napadi, podatki_poskodbe)
+podatki <- right_join(podatki, podatki_zrtve)
+podatki <- filter(podatki, leto==2017)
 
-# Število skupin
-n <- 5
-skupine <- hclust(dist(scale(podatki))) %>% cutree(n)
+vektor <- c("South Asia", "South America", "Western Europe", "World", "Western Sahara", "Sub-Saharan Africa","Southeast Asia", "North America","Middle East & North Africa", "Eastern Europe", "East Asia", "Central America & Caribbean",  "Central Asia", "Australasia & Oceania")
+i <- 1
+while (i<=length(vektor)) {
+  podatki <- filter(podatki, drzava != vektor[i])
+  i <- i +1
+}
+
+podatki$drzava <- gsub("United States", "United States of America",podatki$drzava)
+podatki$drzava <- gsub("Tanzania", "United Republic of Tanzania",podatki$drzava)
+podatki$drzava <- gsub("Democratic Republic of Congo", "Democratic Republic of the Congo",podatki$drzava)
+podatki$drzava <- gsub("Czech Republic", "Czechia",podatki$drzava)
+podatki$drzava <- gsub("Cote d'Ivoire", "Ivory Coast",podatki$drzava)
+podatki$drzava <- gsub("Serbia", "Republic of Serbia",podatki$drzava)
+
+podatki$leto <- NULL
+row.names(podatki) <- podatki$drzava
+podatki$drzava <- NULL
+podatki.norm <- podatki %>% scale()
+
+
+k <- kmeans(podatki.norm, 5, nstart=1000)
+skupine <- data.frame(drzave=row.names(podatki.norm), skupine=factor(k$cluster))
+zemljevid_sveta <- ggplot(right_join(zemljevid,skupine, by=c("SOVEREIGNT"="drzave"))) + geom_polygon() + 
+  aes(x=long, y=lat, group=group, fill=skupine) + xlab("") + ylab("") + theme_minimal()
--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -4,7 +4,7 @@
 graf.svetovni.napadi <- ggplot(podatki_napadi %>% filter(drzava == "World")) + aes(x=leto, y=stevilo, group=drzava)+ geom_point() + geom_line() + theme(panel.grid.major.y =element_line(color="red"))
 
 #graf evropski napadi
-graf.evropa.napadi <- ggplot(podatki_napadi %>% filter(drzava %in% c("Western Europe", "Eastern Europe"))) + aes(x=leto, y=stevilo, group=drzava) + geom_line(color="red")
+graf.evropa.napadi <- ggplot(podatki_napadi %>% filter(drzava %in% c("Western Europe", "Eastern Europe"))) + aes(x=leto, y=stevilo, group=drzava, color=drzava) + geom_line()
 
 #graf napadov v iraqu
 graf.iraq.napadi <- ggplot(podatki_napadi %>% filter(drzava == "Iraq")) + aes(x=leto, y=stevilo, group=drzava) + geom_line() + geom_smooth()
@@ -21,9 +21,9 @@ pomozna_tabela1 <- right_join(pomozna_tabela1, podatki_napadi %>% filter(drzava 
 pomozna_tabela1$drzava <- NULL
 names(pomozna_tabela1)[names(pomozna_tabela1)=="stevilo"] <- "stevilo_vzhod"
 
-pomozna_tabela1 <- mutate(pomozna_tabela1, procent = stevilo_vzhod / stevilo_svet)
+pomozna_tabela1 <- mutate(pomozna_tabela1, delez = stevilo_vzhod / stevilo_svet)
 
-graf.vzhod.procenti <- ggplot(pomozna_tabela1) + aes(x=leto, y=procent, group=TRUE) + geom_line() + geom_area()
+graf.vzhod.delez <- ggplot(pomozna_tabela1) + aes(x=leto, y=delez, group=TRUE) + geom_area(fill="red")
 
 
 #graf procenti zahodno Evropskih drzav
@@ -37,15 +37,25 @@ pomozna_tabela2 <- right_join(pomozna_tabela2, podatki_napadi %>% filter(drzava 
 pomozna_tabela2$drzava <- NULL
 names(pomozna_tabela2)[names(pomozna_tabela2)=="stevilo"] <- "stevilo_evropa"
 
-pomozna_tabela2 <- mutate(pomozna_tabela2, procent = stevilo_evropa / stevilo_svet)
+pomozna_tabela2 <- mutate(pomozna_tabela2, delez = stevilo_evropa / stevilo_svet)
 
-graf.evropa.procenti <- ggplot(pomozna_tabela2) + aes(x=leto, y=procent, group=TRUE) + geom_line() + geom_area()
+graf.evropa.delez <- ggplot(pomozna_tabela2) + aes(x=leto, y=delez, group=TRUE) + geom_area(fill="red")
 
 
 #graf žrtev proti poškodovanim
 
 tabela_zrtev_poskodovanih <-right_join(podatki_poskodbe, podatki_zrtve)
 
-graf_zrtev_poskodovanih <- ggplot(tabela_zrtev_poskodovanih %>% filter(drzava %in% c("Iraq", "Western Europe", "Eastern Europe"))) + aes(x=zrtve, y=poskodbe, color=drzava) + geom_point()
+graf_zrtev_poskodovanih_evropa <- ggplot(tabela_zrtev_poskodovanih %>% filter(drzava == "Western Europe")) + aes(x=zrtve, y=poskodbe, color=leto) + geom_point() + geom_smooth(method="lm", formula=y~log(x))
+
+graf_zrtev_poskodovanih_vzhod <- ggplot(tabela_zrtev_poskodovanih %>% filter(drzava == "Middle East & North Africa")) + aes(x=zrtve, y=poskodbe, color=leto) + geom_point() + geom_smooth()
+
+
+
+# Uvozimo zemljevid.
+zemljevid <- uvozi.zemljevid("https://www.naturalearthdata.com/http//www.naturalearthdata.com/download/110m/cultural/110m_cultural.zip",
+                             "ne_110m_admin_0_countries", encoding="UTF-8") %>% fortify
+slikazemljevid <- ggplot(zemljevid) + 
+  geom_polygon(aes(x = long, y = lat, group=group)) 
 
 
